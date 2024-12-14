894300550e24ff970e74573fb0ee05f16a88d585
Eric Duart <eric.duasan@gmail.com>
2024-12-14 19:32:24 +0100

    chore: added missing migration files

---
+++ Migrations/20241214143032_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace robot_generator.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Robots",
+                columns: table => new
+                {
+                    RobotId = table.Column<string>(type: "TEXT", nullable: false),
+                    RobotPath = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Robots", x => x.RobotId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Robots");
+        }
+    }
+}
